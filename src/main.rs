0a0889083625b51d404dedc4873c2e19d4ef0a1f
gSayak <gsayak22@gmail.com>
2023-12-26 23:31:57 +0530

    added response and error

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // Uncomment this block to pass the first stage
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,12 +15,30 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream.write(b"HTTP/1.1 200 OK\r\n\r\nHello, world!").unwrap();
-
+                stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    // let http_request: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
+
+    // println!("Request: {:#?}", http_request);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    println!("Request: {:#?}", request_line);
+    if request_line == "GET / HTTP/1.1" {
+        stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+    } else {
+        stream.write(b"HTTP/1.1 404 NOT FOUND\r\n\r\n").unwrap();
     }
 }
