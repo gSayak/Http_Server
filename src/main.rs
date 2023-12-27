8e0b70a29d678d61d7f4b14dd432e27a8149166f
gSayak <gsayak22@gmail.com>
2023-12-27 21:06:20 +0530

    abc

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     //
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 handle_connection(stream);
             }
             Err(e) => {
@@ -27,12 +27,16 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    println!("Request: {:#?}", request_line);
-    if request_line == "GET / HTTP/1.1" {
-        let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let split_line: Vec<_> = request_line.split(" ").collect();
+    let split_path: Vec<&str> = split_line[1].split("/").collect();
+    if !split_path[2].is_empty() {
+        let response = format!("HTTP/1.1 200 OK\r\n\r\nContent-Type: text/plain\r\n\r\nContent-Length: {}\r\n\r\n{}",split_path[2].len(), split_path[2]);
         stream.write_all(response.as_bytes()).unwrap();
     } else {
         let response = "HTTP/1.1 404 Not Found\r\n\r\n";
         stream.write_all(response.as_bytes()).unwrap();
     }
+    // println!("Path: {:#?}", split_path);
+    // println!("Request: {:#?}", request_line);
+    // println!("Split: {:#?}", split_line);
 }
