73930f3f0a85d991337c5c0c921a95df56990f6d
gSayak <gsayak22@gmail.com>
2024-01-02 17:09:14 +0530

    post

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
     env, fs,
+    fs::File,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
     path::Path,
@@ -26,14 +27,20 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let args: Vec<String> = env::args().collect();
-    let buf_reader = BufReader::new(&mut stream);
+    let mut buf_reader = BufReader::new(&mut stream);
 
+    let buffer = buf_reader.fill_buf().unwrap();
+    // println!("buffer: {:?}", buffer);
+    let result = String::from_utf8(buffer.to_vec()).unwrap();
+
+    // println!("result: {:?}", result);
+    // println!("buf_reader: {:?}", buf_reader);
     let request_line: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
-
+    // println!("request_line: {:?}", request_line);
     let user_agent_header = "User-Agent:";
     let user_agent = request_line
         .iter()
@@ -48,7 +55,7 @@ fn handle_connection(mut stream: TcpStream) {
         let response = "HTTP/1.1 200 OK\r\n\r\n";
         stream.write_all(response.as_bytes()).unwrap();
     }
-    if  method == "GET" && path.starts_with("/echo/") {
+    if method == "GET" && path.starts_with("/echo/") {
         path = path.replace("/echo", "");
         if path.starts_with("/") {
             path.remove(0);
@@ -67,13 +74,13 @@ fn handle_connection(mut stream: TcpStream) {
         );
         stream.write_all(response.as_bytes()).unwrap();
     } else if method == "GET" && path.starts_with("/files") {
-        let filename = args[2].to_string();
+        let dir = args[2].to_string();
         path = path.replace("/files", "");
         if path.starts_with("/") {
             path.remove(0);
         }
         // println!("{} {}", path, filename);
-        let full_path = format!("{filename}/{path}");
+        let full_path = format!("{dir}/{path}");
         if Path::new(&full_path).exists() {
             let contents = fs::read_to_string(&full_path);
             match contents {
@@ -92,12 +99,31 @@ fn handle_connection(mut stream: TcpStream) {
             let response = "HTTP/1.1 404 Not Found\r\n\r\n";
             stream.write_all(response.as_bytes()).unwrap();
         }
-        
-    } else if method == "POST"{
-        println!("POST");
-    }
-    else
-    {
+    } else if method == "POST" && path.starts_with("/files") {
+        let dir = args[2].to_string();
+        path = path.replace("/files", "");
+        if path.starts_with("/") {
+            path.remove(0);
+        }
+        println!("{}", path);
+        // println!("{} {}", path, filename);
+        let full_path = format!("{dir}/{path}");
+        let body_header = "\r\n\r\n";
+        let body: String = result
+            .splitn(2, body_header)
+            .nth(1)
+            .map(|s| s.to_string())
+            .unwrap_or_else(|| "No Body".to_string());
+        if body == "No Body" {
+            let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+            stream.write_all(response.as_bytes()).unwrap();
+        } else {
+            let mut file = File::create(full_path).unwrap();
+            file.write_all(body.as_bytes()).unwrap();
+            let response = format!("HTTP/1.1 201 OK \r\n\r\n");
+            stream.write_all(response.as_bytes()).unwrap();
+        }
+    } else {
         let response = "HTTP/1.1 404 Not Found\r\n\r\n";
         stream.write_all(response.as_bytes()).unwrap();
     }
